8ce5a1408434fff32b9f6a2babacbced73926279
licongxin08 <254987963@qq.com>
2020-08-13 16:58:18 +0800

    '完成了主页的开发'

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$(function () {
+    getUserInfo();
+    var layer = layui.layer;
+    // 点击退出
+    $('#logout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        }
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem('token');
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(name[0].toUpperCase()).show()
+    }
+}
